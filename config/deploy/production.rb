c611ba0238901f17934513930b2980f68e204907
tracymcaruso <tracymcaruso@gmail.com>
2015-08-31 13:36:34 -0600

    cap files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server '107.170.208.129', user: 'deploy', roles: %w{web app}
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
